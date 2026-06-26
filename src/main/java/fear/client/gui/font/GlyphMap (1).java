7e2ca95805056a4a04b5c09c1f5523cd6dcd0c1e
Yukile394 <muhamedmuhet@gmail.com>
2026-06-26 21:02:04 +0300

    Add files via upload

---
+++ src/main/java/fear/client/gui/font/GlyphMap (1).java
@@ -0,0 +1,41 @@
+package fear.client.gui.font;
+
+import net.minecraft.util.Identifier;
+
+import java.awt.Font;
+
+class GlyphMap {
+    final char fromIncl, toExcl;
+    final Font font;
+    final Identifier bindToTexture;
+    final int pixelPadding;
+    int width = 1, height = 1;
+    boolean generated = false;
+
+    public GlyphMap(char from, char to, Font font, Identifier identifier, int padding) {
+        this.fromIncl = from;
+        this.toExcl = to;
+        this.font = font;
+        this.bindToTexture = identifier;
+        this.pixelPadding = padding;
+    }
+
+    public Glyph getGlyph(char c) {
+        return null;
+    }
+
+    public void destroy() {
+        generated = false;
+    }
+
+    public boolean contains(char c) {
+        return c >= fromIncl && c < toExcl;
+    }
+
+    public void generate() {
+        generated = true;
+    }
+
+    public static void registerBufferedImageTexture(Identifier i, java.awt.image.BufferedImage bi) {
+    }
+}
