af4cc04e9974426ddeb2228593e3a3abca6d1ed0
Yukile394 <muhamedmuhet@gmail.com>
2026-06-26 17:53:47 +0300

    Add files via upload

---
+++ src/main/java/fear/client/utility/discord/DiscordRPC.java
@@ -0,0 +1,16 @@
+package fear.client.utility.discord;
+
+public class DiscordRPC {
+    public static final DiscordRPC INSTANCE = new DiscordRPC();
+
+    public void Discord_UpdateHandlers(final DiscordEventHandlers p0) {}
+    public void Discord_UpdatePresence(final DiscordRichPresence p0) {}
+    public void Discord_Respond(final String p0, final int p1) {}
+    public void Discord_Register(final String p0, final String p1) {}
+    public void Discord_Shutdown() {}
+    public void Discord_UpdateConnection() {}
+    public void Discord_RegisterSteamGame(final String p0, final String p1) {}
+    public void Discord_RunCallbacks() {}
+    public void Discord_Initialize(final String p0, final DiscordEventHandlers p1, final boolean p2, final String p3) {}
+    public void Discord_ClearPresence() {}
+}
